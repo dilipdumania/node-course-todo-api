6a1ec84fa55056b03a7076c2571f249ae142b815
dilip.dumania <dilip.dumania@infostretch.com>
2019-09-25 15:39:22 +0530

    Hash User passowrd before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-}
+var password = '123abc';
 
-var token = jwt.sign(data, 'dilip123');
-var decoded = jwt.verify(token, 'dilip123');
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log('Hash : ', hash);
+//     });
+// });
 
-console.log(`Token : ${token}`);
-console.log('tring decoded :', decoded);
+var hashPass = '$2a$10$7Krn0Or0iHUEnygyhUA1TOI36b4m6KvqLBUV/wcIhH8n4f21pRjNO';
+
+bcrypt.compare(password, hashPass, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// }
+
+// var token = jwt.sign(data, 'dilip123');
+// var decoded = jwt.verify(token, 'dilip123');
+
+// console.log(`Token : ${token}`);
+// console.log('tring decoded :', decoded);
 
 // var message = 'I am user number 4';
 
